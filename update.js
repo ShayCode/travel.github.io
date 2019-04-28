da7343ad5feb978b762e724f3cbb4e45d26938ec
ShayCode <42371843+ShayCode@users.noreply.github.com>
2019-04-28 11:07:20 -0700

    Update update.js

--- update.js
+++ update.js
@@ -1,2 +1,2 @@
-document.getElementById("updateme").innerHTML = "Hi this is Paradise on Earth !!!!!";  //Change your HTML content here
-document.getElementById("myvideo").src = "https://www.youtube.com/embed/7qStAUJkBv0";  //Change ur yoututbe url here
+//document.getElementById("updateme").innerHTML = "Hi this is Paradise on Earth !!!!!";  //Change your HTML content here
+//document.getElementById("myvideo").src = "https://www.youtube.com/embed/7qStAUJkBv0";  //Change ur yoututbe url here
